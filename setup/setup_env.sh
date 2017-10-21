2df8037f2f24ba906f219af015d82fc423f5932e
Maxwell McKinnon <mmcki@apple.com>
2017-10-21 11:24:24 -0700

    Setup script

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-source ~/anaconda/envs/fastai/bin/activate fastai
+# So it doesn't fork a subshell, run with . ./setup_env.sh
 
+source ~/anaconda/envs/fastai/bin/activate fastai
+aws-get-p2
+aws-start
+aws-ssh
