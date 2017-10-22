4516e4c290b300fb006c39775fa272855cf08a3e
Maxwell McKinnon <mmcki@apple.com>
2017-10-21 18:21:42 -0700

    Fuck - have to switch back to py2.7 for now haha - changed setup script to activate py2.7 env

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # So it doesn't fork a subshell, run with . ./setup_env.sh
 
-source ~/anaconda/envs/fastai/bin/activate fastai
+# source ~/anaconda/envs/fastai/bin/activate fastai # py3.7
+source ~/anaconda/envs/fastai_py27/bin/activate fastai_py27
 aws-get-p2
 aws-start
 aws-ssh
